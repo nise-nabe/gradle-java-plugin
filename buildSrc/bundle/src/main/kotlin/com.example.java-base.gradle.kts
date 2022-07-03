ddeeb5db942d70652b5989b54aca4470e75731b9
nise_nabe <nise.nabe@gmail.com>
2022-07-03 18:34:50 +0900

    try to use raw sourceSets

--- buildSrc/bundle/src/main/kotlin/com.example.java-base.gradle.kts
+++ buildSrc/bundle/src/main/kotlin/com.example.java-base.gradle.kts
@@ -4,9 +4,31 @@ plugins {
 }
 
 sourceSets {
-    create(SourceSet.MAIN_SOURCE_SET_NAME)
+    create(SourceSet.MAIN_SOURCE_SET_NAME) {
+        java.srcDir("src/${SourceSet.MAIN_SOURCE_SET_NAME}/java")
+    }
+    create(SourceSet.TEST_SOURCE_SET_NAME) {
+        java.srcDir("src/${SourceSet.TEST_SOURCE_SET_NAME}/java")
+    }
 }
+
+val mainSourceSet: SourceSet = sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME)
+val testSourceSet: SourceSet = sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME)
 
 configurations {
     create("implementation")
+}
+
+val compileJava = tasks.register<JavaCompile>(mainSourceSet.compileJavaTaskName) {
+    source(mainSourceSet.java)
+    classpath = mainSourceSet.compileClasspath
+}
+
+val compileTestJava = tasks.register<JavaCompile>(testSourceSet.compileJavaTaskName) {
+    source(testSourceSet.java)
+    classpath = testSourceSet.compileClasspath
+}
+
+tasks.build {
+    dependsOn(compileJava, compileTestJava)
 }
