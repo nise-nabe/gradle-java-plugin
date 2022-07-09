04c5d152d04608d2d2ceb631d50b35fb16a7dced
nise_nabe <nise.nabe@gmail.com>
2022-07-09 20:08:56 +0900

    add compile classpath configuration to compile

--- buildSrc/bundle/src/main/kotlin/com.example.java-base.gradle.kts
+++ buildSrc/bundle/src/main/kotlin/com.example.java-base.gradle.kts
@@ -6,7 +6,13 @@ plugins {
 sourceSets {
     create(SourceSet.MAIN_SOURCE_SET_NAME) {
         java.srcDir("src/${SourceSet.MAIN_SOURCE_SET_NAME}/java")
+        compileClasspath = configurations.maybeCreate(compileClasspathConfigurationName).apply {
+            isVisible = false
+            isCanBeResolved = true
+            isCanBeConsumed = false
+        }
     }
+
     create(SourceSet.TEST_SOURCE_SET_NAME) {
         java.srcDir("src/${SourceSet.TEST_SOURCE_SET_NAME}/java")
     }
@@ -22,11 +28,13 @@ configurations {
 val compileJava = tasks.register<JavaCompile>(mainSourceSet.compileJavaTaskName) {
     source(mainSourceSet.java)
     classpath = mainSourceSet.compileClasspath
+    destinationDirectory.set(layout.buildDirectory.dir("classes/${mainSourceSet.java.name}/${mainSourceSet.name}"))
 }
 
 val compileTestJava = tasks.register<JavaCompile>(testSourceSet.compileJavaTaskName) {
     source(testSourceSet.java)
     classpath = testSourceSet.compileClasspath
+    destinationDirectory.set(layout.buildDirectory.dir("classes/${mainSourceSet.java.name}/${mainSourceSet.name}"))
 }
 
 tasks.build {
